af28f8e95a353031ca5a0245aed567b276c080cc
iilinge <kesha.linge@gmail.com>
2020-02-25 21:01:35 +0300

    с трудом вспомнил математику простых чисел.

--- src/main/java/tasks/lesson1/Task1.java
+++ src/main/java/tasks/lesson1/Task1.java
@@ -3,7 +3,37 @@ package tasks.lesson1;
 public class Task1 {
     public static String primeNumbers(int maximum) {
         //TODO: вернуть строку простых чисел которые <= maximum начиная с двойки, разделитель-запятая "2,3,5..."
-        return "";
+        // начало кода Иннокентий
+
+        String envResult = "";
+        String envDetermine = ",";
+
+
+        for (int envNumber = 2; envNumber <= maximum; envNumber++) {
+            boolean envType = true;
+            for (int j = 2; j<envNumber; j++) {
+                if  (envNumber % j == 0) {
+                    envType = false;
+                }
+            }
+            if  (envType == true) {
+                envResult = envResult + envDetermine + envNumber;
+            }
+                envType = true;
+        }
+
+        if (envResult.length() != 0 ) {
+            envResult = envResult.substring(1, envResult.length());
+        }
+
+        System.out.println(envResult);
+
+        return envResult;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(primeNumbers(10));
     }
 
 }
+
