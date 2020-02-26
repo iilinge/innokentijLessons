05e90fcea7b5b88f0a17b8691733d0f4f4417802
DmitryNuzhdin <49698521+DmitryNuzhdin@users.noreply.github.com>
2020-02-26 16:50:54 +0300

    Create PowerOfThreeTest.java

---
+++ src/test/java/tasks/lesson2/PowerOfThreeTest.java
@@ -0,0 +1,25 @@
+package tasks.lesson2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PowerOfThreeTest {
+
+    @Test
+    public void powerOfThree() {
+        assertTrue(PowerOfThree.powerOfThree(1));
+        assertTrue(PowerOfThree.powerOfThree(3));
+        assertTrue(PowerOfThree.powerOfThree(9));
+        assertTrue(PowerOfThree.powerOfThree(27));
+        assertTrue(PowerOfThree.powerOfThree(81));
+        assertFalse(PowerOfThree.powerOfThree(0));
+        assertFalse(PowerOfThree.powerOfThree(2));
+        assertFalse(PowerOfThree.powerOfThree(4));
+        assertFalse(PowerOfThree.powerOfThree(6));
+        assertFalse(PowerOfThree.powerOfThree(33));
+        assertFalse(PowerOfThree.powerOfThree(66));
+        assertFalse(PowerOfThree.powerOfThree(100));
+        assertFalse(PowerOfThree.powerOfThree(123));
+    }
+}
