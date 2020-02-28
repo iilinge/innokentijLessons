7c58e64d14b3ca722dcafc12e42d1a42c99c80b9
iilinge <kesha.linge@gmail.com>
2020-02-29 01:12:52 +0300

    Песочница со списками.

---
+++ src/main/java/lesson3/Lesson3.java
@@ -0,0 +1,38 @@
+package lesson3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lesson3 {
+    public static void main(String[] args) {
+
+
+        int[] array = {0, 2, 9, 234, 7};
+        for (int k=0 ; k < array.length; k++) {
+            System.out.println(array[k] + "\n");
+
+        }
+
+        // сумма элементов массива
+
+        for (int u=0; 0 < array.length; u++) {
+
+           int sumarray = sumarray + array[u];
+        }
+
+        array[0] = 1;
+
+
+        array[4] = 10;
+        System.out.println(array[4]);
+
+
+
+
+    }
+
+
+}
+
